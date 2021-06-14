09f17c0261ea6e494fd0a2e4f3159a596f9f9068
Edwin-1012 <bttagaredwin@gmail.com>
2021-06-13 23:32:30 -0500

    Subiendo nuevos archivos

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('URL','https://localhost/Cafehuertomamey/');
+define('HOST', 'localhost:3307');
+define('DB', 'bdcafetienda');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8');
+?>

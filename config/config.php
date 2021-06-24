acc251092f117631d474df561d24c07aa14dc8a5
IsraelMerlyn <48464995+IsraelMerlyn@users.noreply.github.com>
2021-06-23 19:50:45 -0500

    conexion

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 define('URL','https://localhost/Cafehuertomamey/');
-define('HOST', 'localhost:3307');
-define('DB', 'bdcafetienda');
-define('USER', 'root');
-define('PASSWORD', '');
+define('HOST', 'ec2-52-5-1-20.compute-1.amazonaws.com');
+define('DB', 'd9kea0lq3eg1pv');
+define('USER', 'caysuraaffnjba');
+define('PASSWORD', '1a1d03471e67f993123706220a8642c224ca231799e0389b92e62e78166b13');
 define('CHARSET', 'utf8');
 ?>

4eed0ef68f901cc6b1d091340115cd7200ddf506
Edwin-1012 <bttagaredwin@gmail.com>
2021-07-09 17:21:32 -0500

    Prueba Belen

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 define('URL','https://localhost/Cafehuertomamey/');
 define('HOST', 'ec2-52-5-1-20.compute-1.amazonaws.com');
 define('DB', 'd9kea0lq3eg1pv');
-define('USER', 'caysuraaffnjba');
+define('USER', 'root');
 define('PASSWORD', '1a1d03471e67f993123706220a8642c224ca231799e0389b92e62e78166b13');
 define('CHARSET', 'utf8');
 ?>
